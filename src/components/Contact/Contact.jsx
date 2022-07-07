50d20a1b07abc3a8cf0032604add677a0411d6b0
clin155 <clin7985@gmail.com>
2022-07-06 23:36:58 -0700

    added spin box

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,108 @@
 import "./Contact.scss";
-import background from "./../../static/images/background-min.png";
+import emailjs from '@emailjs/browser'
+import { useRef } from 'react';
+
+import {
+    faPython,
+    faCss3,
+    faGitAlt,
+    faHtml5,
+    faJsSquare,
+    faReact,
+  } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export function Contact(props) {
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+            'service_3p167bj',
+            'template_4a0l0nx',
+            form.current,
+            'NYIsoDXQTIRe-fPHJ'
+            )
+            .then(
+            () => {
+                alert('Message successfully sent!')
+                window.location.reload(false)
+            },
+            () => {
+                alert('Failed to send the message, please try again')
+            }
+            )
+    }
     return (
         <div className='page'>
-            <img className="frontScroll" src={background} alt="backgorund"/>
-            <h1 className="title title2">Contact Me</h1>
+                <div className="contactGrid">
+                    <div className='contactFlex'>
+                        <h1 className="title2">Contact Me</h1>
+                        <p className="pText">I am interested in full-stack internships and co-ops. However, if you have a request or further questions do hesitate to contact me.</p>
+                        <div className="contact-form">
+                            <form ref={form} onSubmit={sendEmail}>
+                            <ul>
+                                <li className="half">
+                                <input placeholder="Name" type="text" name="from_name" required />
+                                </li>
+                                <li className="half">
+                                <input
+                                    placeholder="Email"
+                                    type="email"
+                                    name="email"
+                                    required
+                                />
+                                </li>
+                                <li>
+                                <input
+                                    placeholder="Subject"
+                                    type="text"
+                                    name="subject"
+                                    required
+                                />
+                                </li>
+                                <li>
+                                <textarea
+                                    placeholder="Message"
+                                    name="message"
+                                    required
+                                ></textarea>
+                                </li>
+                                <li>
+                                <input type="hidden" name="to_name" value="Chris" />
+                                <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="stage-cube-cont">
+                        <div className="cubespinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faPython} color="#0753F7" />
+                        </div>
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                        </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
 
-        </div>        
     )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import trees from "./../../static/images/trees-min.png";
 import layer1 from "./../../static/images/layer 1-min.png";
 import layer2 from "./../../static/images/layer 2-min.png";
+import layer3 from "./../../static/images/layer 3-min.png";
 import moon from "./../../static/images/moon-min.png";
 
 export class Home extends React.Component {
@@ -25,10 +26,10 @@ export class Home extends React.Component {
     return (      
     <div className="frontPage" style={{"height": height.toString()+"px"}}>
     <Background classs="frontScroll" />
-    <Layer classs="frontScroll" percent={170} end="+=4000" layer={moon}/>
+    <Layer classs="frontScroll" percent={140} end="+=4000" layer={moon}/>
     {/* <Layer classs="frontScroll" percent={150} end="+=3700" layer={layer4}/> */}
-    {/* <Layer classs="frontScroll" percent={150} end="+=3700" layer={layer3}/> */}
-    <Layer classs="frontScroll"  percent={150} end="+=4000" layer={layer2}/>
+    <Layer classs="frontScroll" percent={180} end="+=3700" layer={layer3}/>
+    <Layer classs="frontScroll"  percent={160} end="+=4000" layer={layer2}/>
     <Layer classs="frontScroll" percent={250} end="+=8000" layer={layer1}/>
     <Trees classs="frontScrollTrees" handleResize={this.handleResize}/>
     <h1 className='title'>Hi,<br></br>I'm Chris.</h1>
