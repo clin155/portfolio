db28422f4dc6f93b791acd9ebb9b2e6adecface1
clin155 <clin7985@gmail.com>
2022-07-06 20:30:51 -0700

    fixed spacing

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,11 @@
 import "./Contact.scss";
-import trees from "./../../static/images/trees.png";
 import background from "./../../static/images/background-min.png";
 
 export function Contact(props) {
     return (
         <div className='page'>
             <img className="frontScroll" src={background} alt="backgorund"/>
-            <h1 className="title title2">Contact <br></br>&amp; Me</h1>
-            <img className='frontScroll thirdTrees' src={trees} alt="background"></img>
+            <h1 className="title title2">Contact Me</h1>
 
         </div>        
     )
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -5,7 +5,7 @@ import { SkillsTable } from "./";
 export function Skills(props) {
     return (
         <div className='page3'>
-            <h1 className="smallerTitle">Skills <br></br>&amp; Experience</h1>
+            <h1 className="topSkill">Skills <br></br>&amp; Experience</h1>
             {/* <img className='frontPage secTrees' src={tzees} alt="background"></img> */}
             <SkillsTable />
         </div>   
