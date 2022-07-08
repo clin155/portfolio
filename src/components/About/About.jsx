9f0ac7b3617afdd45a0729497c9dd8e9ba65520e
clin155 <clin7985@gmail.com>
2022-07-07 19:46:44 -0700

    static done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,8 +6,14 @@ import trees from "./../../static/images/trees-min.png";
 export function About(props) {
     return (
         <div className='page'>
-            <h1 className="smallerTitle">About Me</h1>
+            <div className="skillFlex">
+                <h1 className="smallerTitle">About Me</h1>
+                <p className="pText3">I am a junior at the University of Michigan majoring in Computer Science. I love everything to do with tech and
+            software, but I am particularly interested in full-stack development and machine learning. When I'm not coding, I can be found running, playing the piano, or listening to music.</p>
+            </div>
+
             <img className='secTrees' src={trees} alt="background"></img>
+
             {/* <Scroller img={trees} spd={1} classs="flip"/> */}
         </div>        
     )
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -5,7 +5,12 @@ import { SkillsTable } from "./";
 export function Skills(props) {
     return (
         <div className='page3'>
-            <h1 className="topSkill">Skills &amp; Experience</h1>
+            <div className="skillFlex">
+                <h1 className="topSkill">Skills &amp; Experience</h1>
+                <p className="pText2">On the right are all the languages and technologies I have used. The list is mostly in descending order of
+                    proficiency. These skills include Python, Javascript, C++, React, Node, Sass, Git, Docker and more. You can download my resume
+                    <a href="/resume.pdf"> here.</a></p>
+            </div>
             {/* <img className='frontPage secTrees' src={tzees} alt="background"></img> */}
             <SkillsTable />
         </div>   
