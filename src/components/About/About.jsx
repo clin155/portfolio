06cd2fadac155bd3348a00bede96bc81c022c8df
clin155 <clin7985@gmail.com>
2022-09-01 13:00:04 -0400

    pic added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,14 +2,18 @@ import "./../App.scss";
 import trees from "./../../static/images/trees-min.png";
 
 // import { Scroller } from './../Scroller/Scroller.jsx'
+import pic from "./../../static/images/pic.jpg";
 
 export function About(props) {
     return (
         <div className='page'>
-            <div className="skillFlex">
-                <h1 className="smallerTitle">About Me</h1>
-                <p className="pText3">I am a junior at the University of Michigan majoring in Computer Science. I love everything to do with tech and
-            software, but I am particularly interested in full-stack development and machine learning. When I'm not coding, I can be found running, playing the piano, or listening to music.</p>
+            <div className="gorker">
+                <div className="skillFlex">
+                    <h1 className="smallerTitle">About Me</h1>
+                    <p className="pText3">I am a junior at the University of Michigan majoring in Computer Science. I love everything to do with tech and
+                software, but I am particularly interested in full-stack development and machine learning. When I'm not coding, I can be found running, playing the piano, or listening to music.</p>
+                </div>
+                <img src={pic} className="pic" alt="gork"></img>
             </div>
 
             <img className='secTrees' src={trees} alt="background"></img>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,6 +1,8 @@
 import "./Skills.scss";
 
 import { SkillsTable } from "./";
+import github from "./../../static/images/github.png";
+import linkedin from "./../../static/images/linkedin.png";
 
 export function Skills(props) {
     return (
@@ -10,6 +12,10 @@ export function Skills(props) {
                 <p className="pText2">On the right are all the languages and technologies I have used. The list is mostly in descending order of
                     proficiency. These skills include Python, Javascript, C++, React, Node, Sass, Git, Docker and more. You can download my resume
                     <a href="/portfolio/resume.pdf"> here.</a></p>
+                <div className="icons">
+                    <a href="https://github.com/clin155"><img className="ic" src={github} alt="gork"></img></a>
+                    <a href="https://www.linkedin.com/in/christopher-lin-5220a6202/"><img className="ic" src={linkedin} alt="gork"></img></a>
+                </div>
             </div>
             {/* <img className='frontPage secTrees' src={tzees} alt="background"></img> */}
             <SkillsTable />
